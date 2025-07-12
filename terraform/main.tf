849b1e3f05bff9325c142635afe59f82bab24f35
Dhanraj Sharma <dhanraj.sharma@sarvika.com>
2025-07-12 20:24:42 +0530

    updated terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,20 @@
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
-  name = "aryaxai-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
   azs  = ["${var.region}a", "${var.region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = var.private_subnets_cidr
+  public_subnets  = var.public_subnets_cidr
   enable_nat_gateway = true
   single_nat_gateway = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 module "eks" {
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,39 @@
+variable "profile" {
+  default = "ssmz-qa"
+}
+
 variable "region" {
   description = "AWS region"
-  default     = "us-west-2"
+  type = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_name" {
+  description = "AWS VPC Name"
+  type = string
+  default = "aryaxai-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "AWS VPC CIDR"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidr" {
+  description = "VPC public subnet CIDRs"
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "private_subnets_cidr" {
+  description = "VPC public subnet CIDRs"
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "cluster_name" {
   description = "EKS cluster name"
+  type = string
   default     = "aryaxai-eks"
 }
